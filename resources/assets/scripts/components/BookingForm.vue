2fbd55fb1a1b29a3949b722257aec479ac9b4986
programmer-man <bryan@kerigan.com>
2018-08-28 13:40:38 -0400

    tab 1 syule

--- resources/assets/scripts/components/BookingForm.vue
+++ resources/assets/scripts/components/BookingForm.vue
@@ -1,43 +1,77 @@
 <template>
     <div>
         <form>
-            <nav class="navbar navbar-light">
-                <button class="btn btn-outline-secondary" type="button" :class="{'btn-outline-success': step == 1, 'btn-sm': step !== 1}" @click="step = 1">Step 1 - Trip Details</button>
-                <button class="btn btn-outline-secondary" type="button" :class="{'btn-outline-success': step == 2, 'btn-sm': step !== 2}" @click="step = 2">Step 2 - Contact Info</button>
-                <button class="btn btn-outline-secondary" type="button" :class="{'btn-outline-success': step == 3, 'btn-sm': step !== 3}" @click="step = 3">Step 3 - Payment</button>
-                <button class="btn btn-outline-secondary" type="button" :class="{'btn-outline-success': step == 4, 'btn-sm': step !== 4}" @click="step = 4">Step 4 - Billing</button>
-                <button class="btn btn-outline-secondary" type="button" :class="{'btn-outline-success': step == 5, 'btn-sm': step !== 5}" @click="step = 5">Step 5 - Confirmation</button>
-            </nav>
+            <div class="px-3">
+                <div class="row justify-content-between">
+                    <button class="btn col-md-auto" type="button" :class="{'btn-primary text-white': step == 1, 'btn-outline-secondary': step !== 1}" @click="step = 1">Step 1 - Trip Details</button>
+                    <button class="btn col-md-auto" type="button" :class="{'btn-primary text-white': step == 2, 'btn-outline-secondary': step !== 2}" @click="step = 2">Step 2 - Contact Info</button>
+                    <button class="btn col-md-auto" type="button" :class="{'btn-primary text-white': step == 3, 'btn-outline-secondary': step !== 3}" @click="step = 3">Step 3 - Payment</button>
+                    <button class="btn col-md-auto" type="button" :class="{'btn-primary text-white': step == 4, 'btn-outline-secondary': step !== 4}" @click="step = 4">Step 4 - Billing</button>
+                    <button class="btn col-md-auto" type="button" :class="{'btn-primary text-white': step == 5, 'btn-outline-secondary': step !== 5}" @click="step = 5">Step 5 - Confirmation</button>
+                </div>
+            </div>
             <div v-if="step == 1">
-                <HotelDatePicker 
-                    :startingDateValue="checkIn" 
-                    :endingDateValue="checkOut" 
-                    format="MM/DD/YYYY" 
-                    :minNights=7
-                    @checkInChanged="checkInChanged"
-                    @checkOutChanged="checkOutChanged"
-                >
-                </HotelDatePicker>
+                <div class="row mt-4">
+                    <div class="col-md-6">
+                        <label>Trip Schedule</label>
+                        <div class="form-group">
+                            <HotelDatePicker 
+                                :startingDateValue="checkIn" 
+                                :endingDateValue="checkOut" 
+                                format="MM/DD/YY" 
+                                :minNights=7
+                                @checkInChanged="checkInChanged"
+                                @checkOutChanged="checkOutChanged"
+                            >
+                            </HotelDatePicker>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="numNights">Number of Nights</label>
+                        <div class="form-group">
+                            <input type="number" id="numNights" class="form-control" v-model="numNights" disabled>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="numPersons">Number of people</label>
+                        <div class="form-group">
+                            <input type="number" id="numPersons" class="form-control" v-model="info.Persons">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    
+                    <div class="col-md-4">
+                        <label for="email">Email</label>
+                        <div class="form-group">
+                            <input id="email" class="form-control" type="email" name="email" v-model="info.Email">
+                        </div>
+                        <label for="email_confirmation">Confirm your Email</label>
+                        <div class="form-group">
+                            <input id="email_confirmation" class="form-control" type="email" name="email_confirmation" v-model="email">
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <label for="comments">Comments</label>
+                        <div class="form-group">
+                            <textarea id="comments" class="form-control" v-model="info.Notes" style="height:145px;"></textarea>
+                        </div>
+                    </div>
+                </div>
+
                 <input type="hidden" v-model="info.UnitId">
                 <input type="hidden" v-model="info.LocationId">
-                <label for="numPersons">Number of people</label>
-                <input type="number" id="numPersons" v-model="info.Persons">
-                <label for="numNights">Number of Nights</label>
-                <input type="number" id="numNights" v-model="numNights" disabled>
-                <label for="comments">Comments</label>
-                <textarea id="comments" v-model="info.Notes"></textarea>
-                <label for="email">
-                    Email
-                </label>
-                <input id="email" type="email" name="email" v-model="info.Email">
-                <label for="email_confirmation">
-                    Confirm your Email
-                </label>
-                <input id="email_confirmation" type="email" name="email_confirmation" v-model="email">
-                <label for="termsAccetped">
-                    Do you accept the terms?
-                </label>
-                <input id="termsAccepted" type="checkbox" name="termsAccepted" v-model="termsAccepted" :checked="termsAccepted">
+                
+                <label>Read our Rental Terms</label>
+                <div class="form-group">
+                    <div id="termstext" class="p-4 border" style="max-height:145px;" v-html="getTerms()"></div>
+                </div>
+                <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                        <input id="termsAccepted" class="custom-control-input" type="checkbox" name="termsAccepted" v-model="termsAccepted" :checked="termsAccepted">
+                        <label class="custom-control-label" for="termsAccetped">Do you accept the terms?</label>
+                    </div>
+                </div>
 
             </div>
             <div v-if="step == 2">
@@ -64,8 +98,8 @@
                 <input type="number" v-model="info.CCCVCode" placeholder="CCCVCode" />
             </div>
         </form>
-        <button @click="back">Back</button>
-        <button @click="next">Next</button>
+        <button class="btn" @click="back">Back</button>
+        <button class="btn btn-primary text-white" @click="next">Next</button>
     </div>
 </template>
 
@@ -89,7 +123,7 @@ export default {
             checkOut: null,
             numNights: 7,
             email: null,
-            termsAccepted: false,
+            termsAccepted: false
         }
     },
     mounted () {
@@ -114,6 +148,9 @@ export default {
         },
         next() {
             if (this.step < 5) this.step += 1;
+        },
+        getTerms() {
+            return '<p>Let\'s just hard code a bunch of terms here. Maybe use Axios?</p><p>blah</p><p>blah</p>';
         }
     }
 }
