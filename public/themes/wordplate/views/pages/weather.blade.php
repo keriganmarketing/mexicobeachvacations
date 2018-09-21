6eddd52c24eeb040923f3d77954d4b347a200f71
programmer-man <bryan@kerigan.com>
2018-09-21 14:52:45 -0400

    fix ie11

--- public/themes/wordplate/views/pages/weather.blade.php
+++ public/themes/wordplate/views/pages/weather.blade.php
@@ -14,11 +14,11 @@
                     {{ the_content() }}
 
                     <weather-fetcher location="mexico beach, fl" class="weather-page" >
-                        <span slot-scope="{weatherIcon, weatherInfo, loading}">
-                            <div v-if="! loading">
-                                <p class="weather-updated" >This report was generated: <span v-text="weatherInfo.item.pubDate"></span> 
+                        <span slot-scope="slotProps">
+                            <div v-if="! slotProps.loading">
+                                <p class="weather-updated" >This report was generated: <span v-text="slotProps.weatherInfo.item.pubDate"></span> 
                                     using <a href="https://www.yahoo.com/news/weather/country/state/city-2449985/" target="_blank">
-                                    <img style="width:130px;" :src="weatherInfo.image.url" alt="Weather provided by Yahoo! Weather" >
+                                    <img style="width:130px;" src="//l.yimg.com/a/i/brand/purplelogo/uh/us/news-wea.gif" alt="Weather provided by Yahoo! Weather" >
                                 </a></p>
                                 &nbsp;
 
@@ -28,22 +28,22 @@
                                         <div class="row text-center">
                                             <div class="col-sm-6 col-lg-3" >
                                                 <div class="current-conditions text-lg">
-                                                    <i class="wi" :class="weatherIcon"></i>
-                                                    <span v-text="weatherInfo.item.condition.temp + '&deg;F'"></span>
-                                                    <p v-text="weatherInfo.item.condition.text"></p>
+                                                    <i class="wi" :class="slotProps.weatherIcon"></i>
+                                                    <span v-text="slotProps.weatherInfo.item.condition.temp + '&deg;F'"></span>
+                                                    <p v-text="slotProps.weatherInfo.item.condition.text"></p>
                                                 </div>
                                             </div>
                                             <div class="col-sm-6 col-lg-3" >
-                                                <p>Wind Chill: <span v-text="weatherInfo.wind.chill + '&deg;F'" ></span><br>
-                                                Wind Speed: <span v-text="weatherInfo.wind.speed + ' ' + weatherInfo.units.speed" ></span></p>
+                                                <p>Wind Chill: <span v-text="slotProps.weatherInfo.wind.chill + '&deg;F'" ></span><br>
+                                                Wind Speed: <span v-text="slotProps.weatherInfo.wind.speed + ' ' + slotProps.weatherInfo.units.speed" ></span></p>
                                             </div>
                                             <div class="col-sm-6 col-lg-3" >
-                                                <p>Humidity: <span v-text="weatherInfo.atmosphere.humidity + '%'" ></span><br>
-                                                Pressure: <span v-text="weatherInfo.atmosphere.pressure + ' ' + weatherInfo.units.pressure" ></span></p>
+                                                <p>Humidity: <span v-text="slotProps.weatherInfo.atmosphere.humidity + '%'" ></span><br>
+                                                Pressure: <span v-text="slotProps.weatherInfo.atmosphere.pressure + ' ' + slotProps.weatherInfo.units.pressure" ></span></p>
                                             </div>
                                             <div class="col-sm-6 col-lg-3" >
-                                                <p>Sunrise: <span v-text="weatherInfo.astronomy.sunrise" ></span><br>
-                                                Sunset: <span v-text="weatherInfo.astronomy.sunset" ></span></p>
+                                                <p>Sunrise: <span v-text="slotProps.weatherInfo.astronomy.sunrise" ></span><br>
+                                                Sunset: <span v-text="slotProps.weatherInfo.astronomy.sunset" ></span></p>
                                             </div>
                                         </div>
                                     </div>
@@ -52,15 +52,15 @@
 
                                 <h2>Forecast</h2>
                                 <div class="row">
-                                    <div v-for="item in weatherInfo.item.forecast" :key="item.key" class="col-6 col-md-4 col-lg-3 col-xl-2 mb-4">
+                                    <div v-for="item in slotProps.weatherInfo.item.forecast" :key="item.key" class="col-6 col-md-4 col-lg-3 col-xl-2 mb-4">
                                         <div class="card text-center">
                                             <div class="card-body">
-                                        <p><span v-text="item.day"></span><br>
-                                        <span v-text="item.date" style="font-size:.9em;" class="text-dark" ></span></p>
-                                        <p><weather-icon :code="item.code"></weather-icon></p>
-                                        High: <span v-text="item.high + '&deg;F'"></span><br>
-                                        Low: <span v-text="item.low + '&deg;F'"></span><br>
-                                        <span v-text="item.text"></span></p>
+                                                <p><span v-text="item.day"></span><br>
+                                                <span v-text="item.date" style="font-size:.9em;" class="text-dark" ></span></p>
+                                                <p><weather-icon :code="item.code"></weather-icon></p>
+                                                High: <span v-text="item.high + '&deg;F'"></span><br>
+                                                Low: <span v-text="item.low + '&deg;F'"></span><br>
+                                                <span v-text="item.text"></span></p>
                                             </div>
                                         </div>
                                     </div>
